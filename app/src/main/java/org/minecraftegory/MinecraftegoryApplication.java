9b23baf0237a42ec372bf1a24db7dc7ddd36fcfc
Hortense Cauchois <hortensecauchois@gmail.com>
2025-01-05 19:44:39 +0100

    feat: MinecraftegoryApplication.java

---
+++ app/src/main/java/org/minecraftegory/MinecraftegoryApplication.java
@@ -0,0 +1,13 @@
+package org.minecraftegory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftegoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinecraftegoryApplication.class, args);
+    }
+
+}
