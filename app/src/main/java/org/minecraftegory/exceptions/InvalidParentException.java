f9ff09f642b9f8d32bc29cff47633a4b7f6fc125
Hortense Cauchois <hortensecauchois@gmail.com>
2025-01-10 08:42:32 +0100

    feat: add InvalidParentException

---
+++ app/src/main/java/org/minecraftegory/exceptions/InvalidParentException.java
@@ -0,0 +1,7 @@
+package org.minecraftegory.exceptions;
+
+public class InvalidParentException extends Exception {
+    public InvalidParentException(String message) {
+        super(message);
+    }
+}
