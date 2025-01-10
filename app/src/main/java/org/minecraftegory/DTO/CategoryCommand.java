0b1b49be7f311f22b8d1340a3a8d498643587dec
Hortense Cauchois <hortensecauchois@gmail.com>
2025-01-10 08:42:02 +0100

    feat: add CategoryCommand

---
+++ app/src/main/java/org/minecraftegory/DTO/CategoryCommand.java
@@ -0,0 +1,12 @@
+package org.minecraftegory.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryCommand {
+
+    private String name;
+    private int parentId;
+}
